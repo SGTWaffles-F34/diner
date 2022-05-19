e0a4cb83f94b6fe3fd9a6eaf46e90b917294d9a0
Gavin Sherman <gavinmsherman@gmail.com>
2022-05-19 13:54:02 -0400

    OOP-ifyed my project, to quote Tina

--- index.php
+++ index.php
@@ -4,23 +4,21 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-//Start a session
-session_start();
-
 //Require the necessary files
 require_once('vendor/autoload.php');
-require_once('model/data-layer.php');
-require_once('model/validation.php');
+
+//Start a session
+session_start();
 
 //Create an instance of the Base class
 $f3 = Base::instance();
 
+//create an instance of the controller class
+$con = new Controller($f3);//hand it to our controller class to create an instance
+
 //Define a default route
 $f3->route('GET /', function() {
-    //echo "Diner project";
-
-    $view = new Template();
-    echo $view->render('views/home.html');
+    $GLOBALS['con']->home(); //$GLOBALS gives access to global variables inside a php method
 });
 
 //Define a breakfast route
@@ -49,88 +47,21 @@ $f3->route('GET /breakfast/brunch', function() {
 
 //Define an order route
 $f3->route('GET|POST /order', function($f3) {
-    //echo "Order page";
 
-    //If the form has been submitted
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $GLOBALS['con']->order();
 
-        //Move orderForm1 data from POST to SESSION
-        var_dump ($_POST);
-
-        //Get the data from the post array
-        $food = $_POST['food'];
-        $f3->set('userFood', $food);
-
-        $meal = "";
-        if(isset($_POST['meal'])){
-            $meal = $_POST['meal'];
-        }
-
-        //add the users meal to the hive
-        $f3->set('userMeal', $meal);
-
-        //If data is valid
-        if (validFood($food)) {
-
-            //Store it in the session array
-            $_SESSION['food'] = $food;
-        }
-        //Data is not valid -> store an error message
-        else {
-            $f3->set('errors["food"]', 'Please enter a food at least 2 characters');
-        }
-        if(validMeal($meal)){
-            $_SESSION['meal'] = $meal;
-        }
-        //data is notvalid
-        else {
-            $f3->set('errors["meal"]', 'Meal selection is invalid');
-        }
-
-        //redirect to order2 route if there are no errors
-        if (empty($f3->get('errors'))){
-            header('location: order2');
-        }
-    }
-
-
-    //Add meal data to hive
-    $f3->set('meals', getMeals());
-
-    $view = new Template();
-    echo $view->render('views/orderForm1.html');
 });
 
 //Define an order2 route
 $f3->route('GET|POST /order2', function($f3) {
-    //echo "Order page";
 
-
-
-    //Add condiment data to hive
-    $f3->set('condiments', getCondiments());
-
-    $view = new Template();
-    echo $view->render('views/orderForm2.html');
+    $GLOBALS['con']->order2();
 });
 
 //Define a summary route -> orderSummary.html
 $f3->route('GET|POST /summary', function() {
-    //echo "Order page";
-    var_dump ($_POST);
-    if (empty($_POST['conds'])) {
-        $conds = "none selected";
-    }
-    else {
-        $conds = implode(", ", $_POST['conds']);
-    }
-    $_SESSION['conds'] = $conds;
 
-    $view = new Template();
-    echo $view->render('views/orderSummary.html');
-
-    //Clear the session array
-    session_destroy();
+    $GLOBALS['con']->summary();
 });
 
 //Run fat-free
--- model/data-layer.php
+++ model/data-layer.php
@@ -3,15 +3,17 @@
 /* diner/model/data-layer.php
  * Returns data for the diner app
  */
+class DataLayer {//static methods do not access instance data (fields)
 
-// Get the meals for the order form
-function getMeals()
-{
-    return array("breakfast", "brunch", "lunch", "dinner");
-}
+    // Get the meals for the order form
+    static function getMeals()
+    {
+        return array("breakfast", "brunch", "lunch", "dinner");
+    }
 
-// Get the condiments for the order form
-function getCondiments()
-{
-    return array("ketchup", "mustard", "sriracha", "mayo", "kimchi");
+    // Get the condiments for the order form
+    static function getCondiments()
+    {
+        return array("ketchup", "mustard", "sriracha", "mayo", "kimchi");
+    }
 }
